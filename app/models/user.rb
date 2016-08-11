403bea89c2d4179a6080126f65784a73fb07a2cb
nursetta <n_ursetta@yahoo.com>
2016-08-11 14:50:42 -0600

    test validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
 	has_secure_password
 	has_many :choices
+	validates_presence_of :first_name, :last_name, :location, :party, :email, :profile_pic, :password 
 
   def self.confirm(params)
     @user = User.find_by({email: params[:email]})
